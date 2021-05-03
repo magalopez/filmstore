48d5e5e599f85cb74f768ff73d857aa66d6e8260
magician <mglopeza@indracompany.com>
2021-05-02 21:41:48 -0500

    [ECS][FEAT] add model getFilm

---
+++ src/models/getFilm.js
@@ -0,0 +1,22 @@
+import { HTTP_REQUEST } from './http.js';
+import helpers from '../controller/helpers/helpers.js';
+
+export const GET_FILM = async (URL) => {
+  let result;
+
+  await HTTP_REQUEST(URL).then(resp => {
+    const { Response : success, Search : data, totalResults } = resp;    
+
+    if(success && data) {
+      result = data.map((element) => {
+        element["price"] = helpers.randomNumber(50);
+        return element;
+      });
+      result['totalResults'] = totalResults;
+    }
+    else if (success) {
+      result = resp;
+    }
+  });
+  return result;
+};
